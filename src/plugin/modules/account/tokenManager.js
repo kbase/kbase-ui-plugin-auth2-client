ea7d64d8b1da649e9b12f33cd8a90c1adb47a7ed
Erik Pearson <eapearson@lbl.gov>
2017-03-14 16:06:58 -0700

    Add example implementation of token revocation to exercise the api

--- src/plugin/modules/account/tokenManager.js
+++ src/plugin/modules/account/tokenManager.js
@@ -1,8 +1,12 @@
 /*global Promise*/
 define([
     'kb_common/html',
+    'kb_common/domEvent2',
+    'kb_common/bootstrapUtils'
 ], function (
-    html
+    html,
+    DomEvent,
+    BS
 ) {
     var // t = html.tagMaker(),
         t = html.tag,
@@ -30,6 +34,11 @@ define([
 
         var vm = {
             roles: {
+                value: null
+            },
+            toolbar: {
+                id: html.genId(),
+                enabled: false,
                 value: null
             },
             serverTokens: {
@@ -63,6 +72,7 @@ define([
             bindVmNode(vm.developerTokens);
             bindVmNode(vm.allTokens);
             bindVmNode(vm.currentToken);
+            bindVmNode(vm.toolbar);
         }
 
         function renderLayout() {
@@ -77,11 +87,20 @@ define([
                 }, [
                     div({ class: 'col-md-12' }, [
                         div({
-                            id: vm.currentToken.id
+                            id: vm.toolbar.id
                         }),
-                        div({
-                            id: vm.allTokens.id
-                        }),
+                        BS.buildPanel({
+                            title: 'Session Tokens',
+                            body: div({
+                                id: vm.currentToken.id
+                            })
+                        }),  
+                        BS.buildPanel({
+                            title: 'Session Tokens',
+                            body: div({
+                                id: vm.allTokens.id
+                            })
+                        }),                        
                         div({
                             id: vm.serverTokens.id
                         }),
@@ -96,10 +115,53 @@ define([
 
         function niceDate(epoch) {
             var date = new Date(epoch);
-            return date.toUTCString()
+            return date.toUTCString();
+        }
+
+        function doRevokeToken(tokenId) {
+            // Revoke
+            runtime.service('session').getClient().revokeToken(tokenId)
+                .then(function () {
+                    render();
+                    return null;
+                })
+                .catch(function (err) {
+                    console.error('ERROR', err);
+                });
+        }
+
+        function doLogoutToken(tokenId) {
+            // Revoke
+            return runtime.service('session').getClient().logout(tokenId)
+                .then(function () {
+                    runtime.send('session', 'loggedout');
+                })
+                .catch(function (err) {
+                    console.error('ERROR', err);
+                });
         }
 
         function renderTokens(node, tokens) {
+            var events = DomEvent.make({
+                node: node
+            });
+            var revokeAllButton;
+            if (tokens.length > 0) {
+                revokeAllButton = button({
+                    type: 'button',
+                    class: 'btn btn-danger',
+                    id: events.addEvent({
+                        type: 'click',
+                        handler: doRevokeAll
+                    })
+                }, 'Revoke All');
+            } else {
+                revokeAllButton = button({
+                    type: 'button',
+                    class: 'btn btn-danger',
+                    disabled: true
+                }, 'Revoke All');
+            }
             node.innerHTML = table({
                 class: 'table table-striped'
             }, [
@@ -108,7 +170,8 @@ define([
                     th('Expires'),
                     th('Type'),
                     th('User'),
-                    th('Id')
+                    th('Id'),
+                    th(revokeAllButton)
                 ])
             ].concat(tokens.map(function (token) {
                 return tr([
@@ -116,9 +179,111 @@ define([
                     td(niceDate(token.expires) + '<br>' + token.expires),
                     td(token.type),
                     td(token.user),
-                    td(token.id)
+                    td(token.id),
+                    td(button({
+                        class: 'btn btn-danger',
+                        type: 'button',
+                        id: events.addEvent({
+                            type: 'click',
+                            handler: function () {
+                                doRevokeToken(token.id);
+                            }
+                        })
+                    }, 'Revoke'))
                 ]);
             })));
+            events.attachEvents();
+        }
+
+        function renderCurrentTokens(node, tokens) {
+            var events = DomEvent.make({
+                node: node
+            });
+            node.innerHTML = table({
+                class: 'table table-striped'
+            }, [
+                tr([
+                    th('Created'),
+                    th('Expires'),
+                    th('Type'),
+                    th('User'),
+                    th('Id'),
+                    th('')
+                ])
+            ].concat(tokens.map(function (token) {
+                return tr([
+                    td(niceDate(token.created)),
+                    td(niceDate(token.expires) + '<br>' + token.expires),
+                    td(token.type),
+                    td(token.user),
+                    td(token.id),
+                    td([
+                        button({
+                            class: 'btn btn-danger',
+                            type: 'button',
+                            id: events.addEvent({
+                                type: 'click',
+                                handler: function () {
+                                    doLogoutToken(token.id);
+                                }
+                            })
+                        }, 'Logout')
+                    ])
+                ]);
+            })));
+            events.attachEvents();
+        }
+
+        function doRevokeAllAndLogout() {
+            return doRevokeAll()
+                .then(function () {
+                    // sloppy, but conserves code.
+                    return doLogoutToken(null);
+                });
+        }
+
+        function doRevokeAll() {
+            return runtime.service('session').getClient().getTokens()
+                .then(function (result) {
+                    return Promise.all(result.tokens.map(function (token) {
+                        return runtime.service('session').getClient().revokeToken(token.id);
+                    }));
+                })
+                .then(function () {
+                    render();
+                    return null;
+                })
+                .catch(function (err) {
+                    console.error('ERROR', err);
+                });
+        }
+
+        function renderToolbar() {
+            var events = DomEvent.make({
+                node: container
+            });
+            vm.toolbar.node.innerHTML = div({
+                class: 'btn-toolbar',
+                role: 'toolbar',
+                style: {
+                    margin: '10px 0 10px 0'
+                }
+            }, [
+                div({
+                    class: 'btn-group pull-right',
+                    role: 'group'
+                }, [
+                    button({
+                        type: 'button',
+                        class: 'btn btn-danger',
+                        id: events.addEvent({
+                            type: 'click',
+                            handler: doRevokeAllAndLogout
+                        })
+                    }, 'Revoke All and Logout')
+                ])
+            ]);
+            events.attachEvents();
         }
 
         function renderAllTokens() {
@@ -126,27 +291,49 @@ define([
                 runtime.service('session').getClient().getTokens()
                     .then(function (result) {
 
+                        renderToolbar();
+
                         // Render "current" token.
-                        renderTokens(vm.currentToken.node, [result.current]);
+                        renderCurrentTokens(vm.currentToken.node, [result.current]);
 
                         // Render "other" tokens
                         renderTokens(vm.allTokens.node, result.tokens);
-                       
+
                     })
                     .catch(function (err) {
                         vm.serverTokens.node.innerHTML = 'Sorry, error, look in console: ' + err.message;
                     });
             }
         }
+
         function renderServerTokens() {
             if (vm.serverTokens.enabled) {
                 vm.serverTokens.node.innerHTML = 'enabled';
             }
         }
+
         function renderDeveloperTokens() {
             if (vm.developerTokens.enabled) {
                 vm.developerTokens.node.innerHTML = 'enabled';
             }
+        }
+
+        function render() {
+            return runtime.service('session').getClient().getAccount()
+                .then(function (account) {
+                    vm.roles.value = account.roles;
+                    vm.roles.value.forEach(function (role) {
+                        switch (role.id) {
+                        case 'ServToken':
+                            vm.serverTokens.enabled = true;
+                            break;
+                        case 'DevToken':
+                            vm.developerTokens.enabled = true;
+                            break;
+                        }
+                    });
+                    return Promise.all([renderAllTokens(), renderServerTokens(), renderDeveloperTokens()]);
+                });
         }
 
         function attach(node) {
@@ -159,21 +346,7 @@ define([
         function start(params) {
             return Promise.try(function () {
                 renderLayout();
-                return runtime.service('session').getClient().getAccount()
-                    .then(function (account) {
-                        vm.roles.value = account.roles;
-                        vm.roles.value.forEach(function (role) {
-                            switch (role.id) {
-                            case 'ServToken':
-                                vm.serverTokens.enabled = true;
-                                break;
-                            case 'DevToken':
-                                vm.developerTokens.enabled = true;
-                                break;
-                            }
-                        });
-                        return [renderAllTokens(), renderServerTokens(), renderDeveloperTokens()];
-                    });
+                render();
             });
         }
 
@@ -188,7 +361,7 @@ define([
                     hostNode.innerHTML = '';
                 }
             });
-        } 
+        }
 
         return {
             attach: attach,
