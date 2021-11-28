a60c91a123cd377577c3f3fbe849ded46ee36753
Erik Pearson <eapearson@lbl.gov>
2021-11-28 10:48:10 -0800

    clean up code

--- src/plugin/iframe_root/modules/widgets/tabsWidget.js
+++ src/plugin/iframe_root/modules/widgets/tabsWidget.js
@@ -4,41 +4,24 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
     const t = html.tag,
         div = t('div');
 
-    function factory(config) {
-        var hostNode,
-            runtime = config.runtime;
+    function make(config) {
+        let hostNode;
+        const runtime = config.runtime;
 
-        // var tabsControl = document.querySelector(selector);
+        let tabSet;
+        let currentTab;
 
-        var tabSet;
-        var currentTab;
-
-        // var tabsOrder = arg.order;
-        // var tabsDefinition = arg.tabs;
-
-        var tabsId = html.genId();
-        var tabsMap = config.tabs.reduce(function (accum, tab) {
+        const tabsId = html.genId();
+        const tabsMap = config.tabs.reduce(function (accum, tab) {
             if (tab) {
                 accum[tab.name] = tab;
             }
             return accum;
         }, {});
 
-        //     function render() {
-        //         var html = '<div class="flex-tabs" id="' + myId + '">' +
-        //                    ''
-
-        //         tabsOrder.map(function (tabName) {
-        //             var tab = tabsDefinition[tabName];
-
-        //         }
-        // }
-
         function render() {
-            var style = {};
-            if (config.style) {
-                style = config.style;
-            }
+            const style = config.style ? config.style : {};
+
             hostNode.innerHTML = div(
                 {
                     class: 'flex-tabs',
@@ -89,15 +72,15 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
             if (!currentTab) {
                 return;
             }
-            var tab = hostNode.querySelector('[data-tab="' + currentTab + '"]');
-            var panel = hostNode.querySelector('[data-tab-panel="' + currentTab + '"]');
+            const tab = hostNode.querySelector('[data-tab="' + currentTab + '"]');
+            const panel = hostNode.querySelector('[data-tab-panel="' + currentTab + '"]');
             tab.classList.add('-inactive');
             panel.classList.add('-inactive');
             tab.classList.remove('-active');
             panel.classList.remove('-active');
         }
 
-        var currentTabWidget;
+        let currentTabWidget;
 
         function unloadCurrentTab() {
             return Promise.try(function () {
@@ -124,7 +107,7 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
                 throw new Error('No panel factory or class');
             }
 
-            var tabNode = hostNode.querySelector('[data-tab-panel="' + tabName + '"]');
+            const tabNode = hostNode.querySelector(`[data-tab-panel="${tabName}"]`);
             return currentTabWidget.attach(tabNode).then(function () {
                 return currentTabWidget.start();
             });
@@ -132,8 +115,8 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
 
         function activateTab(tabName) {
             currentTab = tabName;
-            var tab = hostNode.querySelector('[data-tab="' + currentTab + '"]');
-            var panel = hostNode.querySelector('[data-tab-panel="' + currentTab + '"]');
+            const tab = hostNode.querySelector(`[data-tab="${currentTab}"]`);
+            const panel = hostNode.querySelector(`[data-tab-panel="${currentTab}"]`);
             return unloadCurrentTab()
                 .then(function () {
                     tab.classList.remove('-inactive');
@@ -154,18 +137,16 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
         function attach(node) {
             return Promise.try(function () {
                 hostNode = node;
-                // More bare to the metal...
                 render();
             });
         }
 
         function start() {
             return Promise.try(function () {
-                var tabs = Array.prototype.slice.call(tabSet.querySelectorAll('[data-tab]'));
-                // var tabPanels = Array.prototype.slice.call(tabSet.querySelectorAll('[data-tab-panel]'));
-                tabs.forEach(function (tab) {
-                    var tabName = tab.getAttribute('data-tab');
-                    var tabPanel = hostNode.querySelector('[data-tab-panel="' + tabName + '"]');
+                const tabs = Array.prototype.slice.call(tabSet.querySelectorAll('[data-tab]'));
+                tabs.forEach((tab) => {
+                    const tabName = tab.getAttribute('data-tab');
+                    const tabPanel = hostNode.querySelector(`[data-tab-panel="${tabName}"]`);
                     tab.classList.add(tabName === currentTab ? '-active' : '-inactive');
                     tabPanel.classList.add(tabName === currentTab ? '-active' : '-inactive');
                     tab.addEventListener('click', function () {
@@ -217,8 +198,6 @@ define(['bluebird', 'kb_lib/html'], function (Promise, html) {
     }
 
     return {
-        make: function (config) {
-            return factory(config);
-        }
+        make
     };
 });
